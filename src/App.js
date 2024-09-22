82d36b6083cbd7acf9d1f337c6eed0c99c56f421
Karan Singh <kvnisad18114982@gmail.com>
2024-09-22 17:25:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UseState from "./useState"
+import UseEffect from "./useEffect"
+import UseReduer from "./useReducer"
+const App = () => {
+  // return <UseReduer />;
+  // return <UseEffect />;
+  return <UseState />;
+};
+
+export default App;
